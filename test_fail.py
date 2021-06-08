b1c6c4773bd546333b6445d35219f2e3ab1165b3
minahonarasa <82671413+minahonarasa@users.noreply.github.com>
2021-06-07 22:03:56 -0600

    Update test_fail.py

--- test_fail.py
+++ test_fail.py
@@ -9,29 +9,29 @@ from main import *
 def input_value(): #keep this fixture for future use, incase any fixture is necessary
     return
 
-#should fail to check if computer choices are valid
+#should fail if computer choices are invalid
 def test_method1():
     assert get_cpu_choice() not in all_usrchoice
 
-#should fail to check functions for the tie situation
+#should fail if functions return invalid flags for the tie situation
 @pytest.mark.parametrize("x,y",[('r','r'),('s','s'),('p','p'),('R','R')])
 def test_method2(x,y):
     assert print_winning_mesg(x,y) ==0 #print_winning_mesg not suppose to say you won
     assert find_winner(x,y) not in status #fail to return acceptable status
     assert find_winner(x,y) == 1 #status is 1 for winner, this should fail
 
-#should fail to check functions if user won, because it did ot run and should not be a wimming message.
+#should fail functions when the user is winner.  The return flag and message should not be a winning message.
 @pytest.mark.parametrize("x,y",[('r','s'),('s','p'),('p','r')])
 def test_method3(x,y):
     assert print_winning_mesg(x,y) ==1 #fail to print you won
     assert find_winner(x,y) == 2 # fail to find the winder flag in find_winner
 
-#should fail to check functions work fine if the user was lost.
+#should fail  if the user lost, but the flag is for the winner.
 @pytest.mark.parametrize("x,y",[('s','r'),('p','s'),('r','p')])
 def test_method4(x,y):
     assert find_winner(x,y) == 0 #wrong flag, the wining flag in find_winner
 
-#should fail to check if wrong input sneaked to the system.
+#should fail if wrong input sneaked to the system.
 @pytest.mark.parametrize("x,y",[('A','a'),('p','k'),('1','2')])
 def test_method5(x,y):
     assert print_winning_mesg(x, y) ==1 #wrong charachters caught
